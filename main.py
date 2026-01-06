bc87cdbfabdd5587e0f75c7fb949e638da43e0fd
Torres-Luis-A <torresla960@gmail.com>
2026-01-05 23:39:52 -0500

    Add usage metadata checks and logging in main function

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     response = client.models.generate_content(
     model='gemini-2.5-flash', 
     contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if response.usage_metadata == None:
+        raise RuntimeError("Usage metadata is None.")
+    else:
+        print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
