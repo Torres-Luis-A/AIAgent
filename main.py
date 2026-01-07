8340ae693fc5c98f1c3e787adf86620a69eb45ed
Torres-Luis-A <torresla960@gmail.com>
2026-01-07 01:04:35 -0500

    added a calculator project with basic operations and JSON output formatting for AIAgent testing.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 
 
@@ -11,16 +14,22 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 def main():
+    parser = argparse.ArgumentParser(description="Generate content using Gemini API.")
+    parser.add_argument('user_prompt', type=str, help="user prompt")
+    parser.add_argument('--verbose', action='store_true', help="Enable verbose output")
+    args = parser.parse_args()
+    messages=[types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    if response.usage_metadata == None:
-        raise RuntimeError("Usage metadata is None.")
+    model='gemini-2.5-flash', contents=messages)
+    if args.verbose:
+        if response.usage_metadata == None:
+            raise RuntimeError("Usage metadata is None.")
+        else:
+            print(f"User prompt:{args.user_prompt}")
+            print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+            print(response.text)
     else:
-        print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-    print(response.text)
-
-
+        print(response.text)
 if __name__ == "__main__":
     main()
