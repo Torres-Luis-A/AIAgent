821d23845d4574cc03ee95e764479cda0c2f8c7b
Torres-Luis-A <torresla960@gmail.com>
2026-01-19 14:11:47 -0500

    added get files info function to retrieve file metadata

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,11 @@
+import os
+
+
 def get_files_info(working_directory, directory="."):
-    pass
+    absolute_path = os.path.abspath(working_directory)
+    target_directory = os.path.normpath(os.path.join(absolute_path, directory))
+    valid_target_dir = os.path.commonpath([absolute_path, target_directory]) == absolute_path
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_directory):
+        return f'Error: The path "{directory}" is not a directory.'
