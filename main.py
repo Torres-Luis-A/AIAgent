d425440da49c989e1892a1817e15cc2d0cd1591e
Torres-Luis-A <torresla960@gmail.com>
2026-02-02 01:53:30 -0500

    Refactor content generation logic and enhance system prompt instructions

--- main.py
+++ main.py
@@ -15,40 +15,47 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 def main():
+
+
     parser = argparse.ArgumentParser(description="Generate content using Gemini API.")
     parser.add_argument('user_prompt', type=str, help="user prompt")
     parser.add_argument('--verbose', action='store_true', help="Enable verbose output")
     args = parser.parse_args()
     messages=[types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model='gemini-2.5-flash',
+
+    print(f"User prompt:{args.user_prompt}")
+    for call in range(20):
+        response = client.models.generate_content(model='gemini-2.5-flash',
                                             contents=messages,
                                             config=types.GenerateContentConfig(system_instruction=system_prompt,temperature=0,tools=[available_functions]))
-    if args.verbose:
-        if response.usage_metadata == None:
-            raise RuntimeError("Usage metadata is None.")
-    print(f"User prompt:{args.user_prompt}")
-    print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-    if not response.function_calls:
-        print(response.text)
-        return
-    else:
-        tools_results = []
-        for function_call in response.function_calls:
-            tool_result = call_function(function_call, verbose=args.verbose)
-            tools_results.append(tool_result)
-            part = tool_result.parts[0]
-            fr= part.function_response
-            data=fr.response
+        for candidate in response.candidates:
+            messages.append(candidate.content)
         if args.verbose:
-            print(f"calling function: {function_call.name}({function_call.args})")
-            print(f"-> {data['result']}")
-        if part.function_response is None or part.function_response.response is None:
-            raise RuntimeError("Function response is None or empty.")
-        if args.verbose:
-            print(f"-> {part.function_response.response}")
-            tools_results.append(part)
-            
-    
+            if response.usage_metadata == None:
+                raise RuntimeError("Usage metadata is None.")
+        
+        print(f"Prompt tokens:{response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+        if not response.function_calls:
+            print(response.text)
+            return
+        else:
+            tools_results = []
+            for function_call in response.function_calls:
+                tool_result = call_function(function_call, verbose=args.verbose)
+                part = tool_result.parts[0]
+                tools_results.append(part)
+                fr= part.function_response
+                data=fr.response
+                if args.verbose:
+                    print(f"calling function: {function_call.name}({function_call.args})")
+                    print(f"-> {data['result']}")
+                if part.function_response is None or part.function_response.response is None:
+                    raise RuntimeError("Function response is None or empty.")
+                if args.verbose:
+                    print(f"-> {part.function_response.response}")
+            messages.append(types.Content(role="user",parts=tools_results))
+    raise RuntimeError("Exceeded maximum number of function call iterations.") 
+
 if __name__ == "__main__":
     main()
--- prompts.py
+++ prompts.py
@@ -7,6 +7,9 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Read file contents
 - Execute Python files with optional arguments
 - Write or overwrite files
+-Do not suggest alternate inputs or corrections to the user prompt.
+- Assume that the code is incorrect before sugeggesting any changes to the input.
+-Alway asume that the user prompt is correct.
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
